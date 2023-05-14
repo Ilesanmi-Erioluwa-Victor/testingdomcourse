5a1b7e1b6df354c05ad879595a522da543af8a4c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-05-14 19:14:02 +0100

    Test passing successfully

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,14 +1,20 @@
-import { render, screen } from "@testing-library/react"
-import Greet from "./Greet"
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
 
 /**
  * Greet component should render the text hello if a name is passed into the component
- * 
+ *
  * It should render hello followed by the name.
  */
 
-test("Greet renders correctly", () =>{
-    render(<Greet />)
-    const textElemet = screen.getByText("Hello");
-    expect(textElemet).toBeInTheDocument()
-})
+test("Greet renders correctly", () => {
+  render(<Greet />);
+  const textElemet = screen.getByText("Hello");
+  expect(textElemet).toBeInTheDocument();
+});
+
+test("Greet renders name when passed as prop", () => {
+  render(<Greet name="Ericjay" />);
+  const textElemet = screen.getByText("Hello Ericjay");
+  expect(textElemet).toBeInTheDocument();
+});
